d81ab1f861dd523c955680867adea77f031ec1d5
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-22 22:20:30 +0600

    es6 intro added

---
+++ basic.js
@@ -0,0 +1,51 @@
+var deposite = 400;
+if (deposite > 500) {
+    
+}
+if (deposite < 500) {
+    
+}
+if (deposite == 500) {
+    
+}
+if (deposite <= 500) {
+    
+}
+if (deposite >= 500) {
+    
+}
+else {
+    
+}
+
+// array
+const numbers = [23, 532, 22, 3566, 899, 1012, 88];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(111);
+numbers[2] = 555;
+if (numbers.indexOf(222) != -1) {
+    
+}
+if (numbers.includes(222)) {
+    
+}
+
+// loop
+// while , for
+for (const number of numbers) {
+    
+}
+
+// function 
+function getFullName(first, second) {
+    const name = first +' ' + second;
+}
+getFullName('Rahim', 'Islam');
+
+// Object
+const bottles = {
+    color: 'yellow',
+    price: '50tk',
+    contains : 'water'
+}
